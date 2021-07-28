b9e4c9102e260e673cc5a5ff361ba7c9512b1cc8
Michelle Fullwood <michelle.fullwood@posh.tech>
2021-07-27 20:28:14 -0400

    Added functionality for actual numerals included and dozens

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Inverse text normalization for numbers. Currently only supports en-US locale.
 
+This fork allows for two additional functionalities:
+- you can intersperse numeric values, e.g. "42 million" instead of only "forty-two million"
+- you can use "dozen" as a unit
+
 ## Usage
 
 Example: `w2n("forty-two hundred and forty-two")`
 
 ## Installation
 
-`pip install words2num`
+`python -m pip install git+https://github.com/michelleful/words2num.git#egg=words2num`
--- words2num/lang_EN_US.py
+++ words2num/lang_EN_US.py
@@ -5,60 +5,68 @@ from decimal import Decimal, localcontext
 
 
 VOCAB = {
-    'zero': (0, 'Z'),
-    'oh': (0, 'Z'),
-    'a': (1, 'A'),
-    'one': (1, 'D'),
-    'two': (2, 'D'),
-    'three': (3, 'D'),
-    'four': (4, 'D'),
-    'five': (5, 'D'),
-    'six': (6, 'D'),
-    'seven': (7, 'D'),
-    'eight': (8, 'D'),
-    'nine': (9, 'D'),
-    'ten': (10, 'M'),
-    'eleven': (11, 'M'),
-    'twelve': (12, 'M'),
-    'thirteen': (13, 'M'),
-    'fourteen': (14, 'M'),
-    'fifteen': (15, 'M'),
-    'sixteen': (16, 'M'),
-    'seventeen': (17, 'M'),
-    'eighteen': (18, 'M'),
-    'nineteen': (19, 'M'),
-    'twenty': (20, 'T'),
-    'thirty': (30, 'T'),
-    'forty': (40, 'T'),
-    'fifty': (50, 'T'),
-    'sixty': (60, 'T'),
-    'seventy': (70, 'T'),
-    'eighty': (80, 'T'),
-    'ninety': (90, 'T'),
-    'hundred': (100, 'H'),
-    'thousand': (10**3, 'X'),
-    'million': (10**6, 'X'),
-    'billion': (10**9, 'X'),
-    'trillion': (10**12, 'X'),
-    'quadrillion': (10**15, 'X'),
-    'quintillion': (10**18, 'X'),
-    'sextillion': (10**21, 'X'),
-    'septillion': (10**24, 'X'),
-    'octillion': (10**27, 'X'),
-    'nonillion': (10**30, 'X'),
-    'decillion': (10**33, 'X'),
-    'undecillion': (10**36, 'X'),
-    'duodecillion': (10**39, 'X'),
-    'tredecillion': (10**42, 'X'),
-    'quattuordecillion': (10**45, 'X'),
-    'quindecillion': (10**48, 'X'),
-    'sexdecillion': (10**51, 'X'),
-    'septendecillion': (10**54, 'X'),
-    'octodecillion': (10**57, 'X'),
-    'novemdecillion': (10**60, 'X'),
-    'vigintillion': (10**63, 'X'),
-    'centillion': (10**303, 'X')
+    "zero": (0, "Z"),
+    "oh": (0, "Z"),
+    "a": (1, "A"),
+    "one": (1, "D"),
+    "two": (2, "D"),
+    "three": (3, "D"),
+    "four": (4, "D"),
+    "five": (5, "D"),
+    "six": (6, "D"),
+    "seven": (7, "D"),
+    "eight": (8, "D"),
+    "nine": (9, "D"),
+    "ten": (10, "M"),
+    "eleven": (11, "M"),
+    "twelve": (12, "M"),
+    "thirteen": (13, "M"),
+    "fourteen": (14, "M"),
+    "fifteen": (15, "M"),
+    "sixteen": (16, "M"),
+    "seventeen": (17, "M"),
+    "eighteen": (18, "M"),
+    "nineteen": (19, "M"),
+    "twenty": (20, "T"),
+    "thirty": (30, "T"),
+    "forty": (40, "T"),
+    "fifty": (50, "T"),
+    "sixty": (60, "T"),
+    "seventy": (70, "T"),
+    "eighty": (80, "T"),
+    "ninety": (90, "T"),
+    "dozen": (12, "H"),
+    "hundred": (100, "H"),
+    "thousand": (10 ** 3, "X"),
+    "million": (10 ** 6, "X"),
+    "billion": (10 ** 9, "X"),
+    "trillion": (10 ** 12, "X"),
+    "quadrillion": (10 ** 15, "X"),
+    "quintillion": (10 ** 18, "X"),
+    "sextillion": (10 ** 21, "X"),
+    "septillion": (10 ** 24, "X"),
+    "octillion": (10 ** 27, "X"),
+    "nonillion": (10 ** 30, "X"),
+    "decillion": (10 ** 33, "X"),
+    "undecillion": (10 ** 36, "X"),
+    "duodecillion": (10 ** 39, "X"),
+    "tredecillion": (10 ** 42, "X"),
+    "quattuordecillion": (10 ** 45, "X"),
+    "quindecillion": (10 ** 48, "X"),
+    "sexdecillion": (10 ** 51, "X"),
+    "septendecillion": (10 ** 54, "X"),
+    "octodecillion": (10 ** 57, "X"),
+    "novemdecillion": (10 ** 60, "X"),
+    "vigintillion": (10 ** 63, "X"),
+    "centillion": (10 ** 303, "X"),
 }
+
+
+def get_value(token):
+    try:
+        return (float(token), "D")
+    except ValueError:
+        return VOCAB[token]
 
 
 class FST:
@@ -76,7 +84,7 @@ class FST:
             return output
 
         def f_mul_hundred(self, n):
-            assert n == 100
+            assert n == 100 or n == 12
             self.value *= n
 
         def f_mul_hundred_and_add(self, n):
@@ -87,40 +95,40 @@ class FST:
             return self.value
 
         self.value = 0
-        self.state = 'S'
+        self.state = "S"
         # self.states = {'S', 'D', 'T', 'M', 'H', 'X', 'Z', 'A', 'F'}
         self.edges = {
-            ('S', 'Z'): f_zero,    # 0
-            ('S', 'D'): f_add,     # 9
-            ('S', 'T'): f_add,     # 90
-            ('S', 'M'): f_add,     # 19
-            ('S', 'A'): f_add,    # 100
-            ('S', 'F'): f_ret,     # 1
-            ('D', 'H'): f_mul_hundred,     # 900
-            ('D', 'X'): f_mul,     # 9000
-            ('D', 'F'): f_ret,     # 9
-            ('T', 'D'): f_add,     # 99
-            ('D', 'T'): f_mul_hundred_and_add,     # 990 (nine ninety)
-            ('D', 'M'): f_mul_hundred_and_add,     # 919 (nine nineteen)
-            ('T', 'H'): f_mul_hundred,
-            ('T', 'X'): f_mul,     # 90000
-            ('T', 'F'): f_ret,     # 90
-            ('M', 'H'): f_mul_hundred,
-            ('M', 'X'): f_mul,     # 19000
-            ('M', 'F'): f_ret,     # 19
-            ('H', 'D'): f_add,     # 909
-            ('H', 'T'): f_add,     # 990
-            ('H', 'M'): f_add,     # 919
-            ('H', 'X'): f_mul,     # 900000
-            ('H', 'F'): f_ret,     # 900
-            ('X', 'D'): f_add,     # 9009
-            ('X', 'T'): f_add,     # 9090
-            ('X', 'M'): f_add,     # 9019
-            ('X', 'F'): f_ret,     # 9000
-            ('Z', 'F'): f_ret,     # 0
-            ('A', 'H'): f_mul_hundred,     # 100
-            ('A', 'X'): f_mul,      # 1000
-            ('A', 'F'): f_ret,      # 1
+            ("S", "Z"): f_zero,  # 0
+            ("S", "D"): f_add,  # 9
+            ("S", "T"): f_add,  # 90
+            ("S", "M"): f_add,  # 19
+            ("S", "A"): f_add,  # 100
+            ("S", "F"): f_ret,  # 1
+            ("D", "H"): f_mul_hundred,  # 900
+            ("D", "X"): f_mul,  # 9000
+            ("D", "F"): f_ret,  # 9
+            ("T", "D"): f_add,  # 99
+            ("D", "T"): f_mul_hundred_and_add,  # 990 (nine ninety)
+            ("D", "M"): f_mul_hundred_and_add,  # 919 (nine nineteen)
+            ("T", "H"): f_mul_hundred,
+            ("T", "X"): f_mul,  # 90000
+            ("T", "F"): f_ret,  # 90
+            ("M", "H"): f_mul_hundred,
+            ("M", "X"): f_mul,  # 19000
+            ("M", "F"): f_ret,  # 19
+            ("H", "D"): f_add,  # 909
+            ("H", "T"): f_add,  # 990
+            ("H", "M"): f_add,  # 919
+            ("H", "X"): f_mul,  # 900000
+            ("H", "F"): f_ret,  # 900
+            ("X", "D"): f_add,  # 9009
+            ("X", "T"): f_add,  # 9090
+            ("X", "M"): f_add,  # 9019
+            ("X", "F"): f_ret,  # 9000
+            ("Z", "F"): f_ret,  # 0
+            ("A", "H"): f_mul_hundred,  # 100
+            ("A", "X"): f_mul,  # 1000
+            ("A", "F"): f_ret,  # 1
         }
 
     def transition(self, token):
@@ -128,8 +136,9 @@ class FST:
         try:
             edge_fn = self.edges[(self.state, label)]
         except KeyError:
-            raise NumberParseException("Invalid number state from "
-                                       "{0} to {1}".format(self.state, label))
+            raise NumberParseException(
+                "Invalid number state from " "{0} to {1}".format(self.state, label)
+            )
         self.state = label
         return edge_fn(self, value)
 
@@ -138,11 +147,12 @@ def compute_placevalues(tokens):
     """Compute the placevalues for each token in the list tokens"""
     pvs = []
     for tok in tokens:
-        if tok == 'point':
+        if tok == "point":
             pvs.append(0)
         else:
-            pvs.append(placevalue(VOCAB[tok][0]))
+            pvs.append(placevalue(get_value(tok)[0]))
     return pvs
+
 
 def tokenize(text):
     tokens = re.split(r"[\s,\-]+(?:and)?", text.lower())
@@ -161,24 +171,22 @@ def tokenize(text):
         # 2: The list of tokens must be longer than one (to prevent extracting all tokens as mul_tokens)
         # 3: The maximum placevalue must be greater than 1 (This limits our mul_tokens to "hundred" or greater)
         while max(pvs) == pvs[-1] and len(pvs) > 1 and max(pvs) > 1:
-            mul_tokens.insert(0, VOCAB[tokens.pop()])
+            mul_tokens.insert(0, get_value(tokens.pop()))
             pvs.pop()
 
         for token in tokens:
-            if token == 'point':
+            if token == "point":
                 if decimal:
-                    raise ValueError("Invalid decimal word "
-                                        "'{0}'".format(token))
+                    raise ValueError("Invalid decimal word " "'{0}'".format(token))
                 else:
                     decimal = True
             else:
                 if decimal:
-                    decimal_tokens.append(VOCAB[token])
+                    decimal_tokens.append(get_value(token))
                 else:
-                    parsed_tokens.append(VOCAB[token])
+                    parsed_tokens.append(get_value(token))
     except KeyError as e:
-        raise ValueError("Invalid number word: "
-                         "{0} in {1}".format(e, text))
+        raise ValueError("Invalid number word: " "{0} in {1}".format(e, text))
     if decimal and not decimal_tokens:
         raise ValueError("Invalid sequence: no tokens following 'point'")
     return parsed_tokens, decimal_tokens, mul_tokens
@@ -196,14 +204,13 @@ def compute(tokens):
         if out:
             outputs.append(out)
             if last_placevalue and last_placevalue <= placevalue(outputs[-1]):
-                raise NumberParseException("Invalid sequence "
-                                           "{0}".format(outputs))
+                raise NumberParseException("Invalid sequence " "{0}".format(outputs))
             last_placevalue = placevalue(outputs[-1])
-    outputs.append(fst.transition((None, 'F')))
+    outputs.append(fst.transition((None, "F")))
     if last_placevalue and last_placevalue <= placevalue(outputs[-1]):
-        raise NumberParseException("Invalid sequence "
-                                   "{0}".format(outputs))
+        raise NumberParseException("Invalid sequence " "{0}".format(outputs))
     return sum(outputs)
+
 
 def compute_multipliers(tokens):
     """
@@ -226,9 +233,8 @@ def compute_decimal(tokens):
         place = -1
         for token in tokens:
             value, label = token
-            if label not in ('D', 'Z'):
-                raise NumberParseException("Invalid sequence after decimal "
-                                           "point")
+            if label not in ("D", "Z"):
+                raise NumberParseException("Invalid sequence after decimal " "point")
             else:
                 total += value * Decimal(10) ** Decimal(place)
                 place -= 1
